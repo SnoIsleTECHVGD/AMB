fb99b39559a1b29c23c280c141279bf0e6a60c8b
MUKILTEO\1714222 <1714222@mukilteo.wednet.edu>
2024-01-03 09:47:28 -0800

    Fixed camera and added the factory asset.

--- Assets/JJ/CameraFollow.cs
+++ Assets/JJ/CameraFollow.cs
@@ -8,14 +8,14 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private float smoothSpeed = 10f; // Adjust the smooth speed as needed
     [SerializeField] private Vector3 offset = new Vector3(0f, 2f, -5f); // Adjust the offset as needed
 
-    void LateUpdate()
+    void FixedUpdate()
     {
         // Calculate the desired position
         Vector3 desiredPosition = target.position + offset;
         desiredPosition.y = transform.position.y;
 
         // Smoothly move the camera to the desired position
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * smoothSpeed);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, Time.fixedDeltaTime * smoothSpeed);
         transform.position = smoothedPosition;
 
         // Set the camera rotation to look at the player
