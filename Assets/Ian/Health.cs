09f7f76cf0d7c19e2482c6adb103c76994c624f8
MUKILTEO\1714222 <1714222@mukilteo.wednet.edu>
2024-01-04 10:02:31 -0800

    heart

---
+++ Assets/Ian/Health.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+
+    public int health;
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    private void Update()
+    {
+        
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            } else
+            {
+                hearts[i].enabled = true;
+            }
+        }
+    }
+}
