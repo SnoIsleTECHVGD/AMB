de9e3d015ea0c05fa686c330ba11e0dead864455
MUKILTEO\1714222 <1714222@mukilteo.wednet.edu>
2024-01-05 08:30:37 -0800

    basic health system in.

--- Assets/Ian/Health.cs
+++ Assets/Ian/Health.cs
@@ -15,15 +15,29 @@ public class Health : MonoBehaviour
 
     private void Update()
     {
+
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
         
         for (int i = 0; i < hearts.Length; i++)
         {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
             if(i < numOfHearts)
             {
                 hearts[i].enabled = true;
             } else
             {
-                hearts[i].enabled = true;
+                hearts[i].enabled = false;
             }
         }
     }
