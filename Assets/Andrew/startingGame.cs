e39b2f37133d5c1f94c614b0e7b53b208cc05dd5
MUKILTEO\1713915 <1713915@Mukilteo.wednet.edu>
2024-01-11 10:06:33 -0800

    Added more for the title

---
+++ Assets/Andrew/startingGame.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Diagnostics;
+using UnityEngine.SceneManagement;
+
+public class startingGame : MonoBehaviour
+{
+    public GameObject title;
+    public static startingGame instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
